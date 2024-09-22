6fcd617d3df7270fcb8ec5a430ea1f2f95755ff9
Samuel Custódio <samuel.rodrigues@aluno.unievangelica.edu.br>
2024-09-22 12:35:26 -0300

    #add:UserClass

--- Lessons/POO/SINACEN/Main.java
+++ Lessons/POO/SINACEN/Main.java
@@ -2,8 +2,15 @@ package POO.SINACEN;
 
 public class Main {
     public static void main(String[] args) {
+        User user = new User();
+        user.exibirDados();
+
+        System.out.println();
+
         PlanoDeTreino planoBasico = new PlanoDeTreino();
         planoBasico.exibirPlano();
+
+        System.out.println();
 
         PlanoDeTreino planoBasico2 = new PlanoDeTreino("Plano de Iniciantes", 6, "Iniciante");
         planoBasico2.exibirPlano();
